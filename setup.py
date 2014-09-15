ef47ae2e361263184cfb9abba4373efbae6598a6
catalyst256 <catalyst256@gmail.com>
2014-09-15 20:22:41 +0100

    initial push

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sniffmypacketsv2',
+    author='catalyst256',
+    version='1.0',
+    author_email='catalyst256@gmail.com',
+    description='Maltego transforms for pcap analysis and more',
+    license='GPL',
+    packages=find_packages('src'),
+    package_dir={ '' : 'src' },
+    zip_safe=False,
+    package_data={
+        '' : [ '*.gif', '*.png', '*.conf', '*.mtz', '*.machine' ] # list of resources
+    },
+    install_requires=[
+        'canari'
+    ],
+    dependency_links=[
+        # custom links for the install_requires
+    ]
+)
